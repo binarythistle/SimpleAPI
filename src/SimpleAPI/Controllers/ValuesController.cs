6f48db8161cbc8467bdf65d404af42ad5c2db05e
les jackson <les.jackson@binarythistle.com>
2019-03-14 19:35:02 +1100

    Fixed unit tests :)

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Max Power";
+            return "Les Jackson";
         }
 
         // POST api/values
