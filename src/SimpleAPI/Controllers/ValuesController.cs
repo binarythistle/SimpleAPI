d61d17a0cbedb5c1ddc5f6ce04d104b96ce6053e
les jackson <les.jackson@binarythistle.com>
2019-03-14 20:16:11 +1100

    changed return values

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SimpleAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "dotnet", "playbook" };
         }
 
         // GET api/values/5
