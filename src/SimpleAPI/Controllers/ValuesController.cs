50ee819c4402f0a00c19c9def0e8f1e71fb29b4b
les jackson <les.jackson@binarythistle.com>
2019-03-14 19:30:23 +1100

    Broken Unit test :(

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Les Jackson";
+            return "Max Power";
         }
 
         // POST api/values
